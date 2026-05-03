a0e2f8bd039195fc665d324cf32603cb1c233a18
Pooja22-05-2003 <studentpooja1234@gmail.com>
2026-05-03 19:36:54 +0530

    For loop java

---
+++ Basics.java
@@ -0,0 +1,8 @@
+public class Basics {
+    public static void main(String[] args) {
+        // for loop starts at 0, runs as long as i is less than or equal to 4
+        for (int i = 0; i <= 4; i++) {
+            System.out.println("Current number: " + i);
+        }
+    }
+}
